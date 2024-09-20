43202be306115dbfa324c36201fba68540e8d0b3
waterrr1389 <riyant258316@gmail.com>
2024-09-20 21:44:10 +0800

    fuck you

--- test_9_14/test_9_14/test.c
+++ test_9_14/test_9_14/test.c
@@ -24,33 +24,44 @@
 //    return 0;
 //}
 
+//#include <stdio.h>
+//int main()
+//{
+//    char arr[13] = { 0 };
+//    int i = 0;
+//    int j = 0;
+//    int tmp = 0;
+//    scanf("%s", arr);
+//    for (i = 0, j = 1; i < 12; i++)
+//    {
+//        if (arr[i] != '-')
+//        {
+//            tmp += (arr[i] - '0') * j;
+//            j++;
+//        }
+//    }
+//    int set = tmp % 11;
+//    char last = (set == 10) ? 'X' : (set + '0');
+//
+//    if (last == arr[12])
+//    {
+//        printf("Right");
+//    }
+//    else
+//    {
+//        arr[12] = last;
+//        printf("%s", arr);
+//    }
+//    return 0;
+//}
+#include<stdlib.h>
+
 #include <stdio.h>
 int main()
 {
-    char arr[13] = { 0 };
-    int i = 0;
-    int j = 0;
-    int tmp = 0;
-    scanf("%s", arr);
-    for (i = 0, j = 1; i < 12; i++)
-    {
-        if (arr[i] != '-')
-        {
-            tmp += (arr[i] - '0') * j;
-            j++;
-        }
-    }
-    int set = tmp % 11;
-    char last = (set == 10) ? 'X' : (set + '0');
-
-    if (last == arr[12])
-    {
-        printf("Right");
-    }
-    else
-    {
-        arr[12] = last;
-        printf("%s", arr);
-    }
-    return 0;
+	char a = -1;
+	unsigned char b = -1;
+	char c = -1;
+	printf("a=%d,b=%d,c=%u", a, b, c);
+	return 0;
 }
